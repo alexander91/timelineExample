f019128b9c9814213509874f478423621e9fd739
a.grishanin <a.grishanin@corp.nekki.ru>
2016-07-03 20:44:56 +0300

    simple example

---
+++ timelineExample/Assets/Scripts/StandardManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class StandardManager : MonoBehaviour {
+    private TimeManager timeManager = new TimeManager();
+
+    // Use this for initialization
+    void Start () {
+
+
+        var allBehs = GetComponentsInChildren<MonoBehaviour>();
+        var allBehsList = new List<MonoBehaviour>(allBehs);
+        var timeDependant = allBehsList.OfType<ITimeChanging>();
+
+        timeManager.TimeDependants = timeDependant;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        timeManager.Time += Time.deltaTime;
+    }
+}
---
+++ timelineExample/Assets/Scripts/TimeManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+public class TimeManager
+{
+    private float time;
+
+    public TimeManager()
+    {
+        TimeDependants = new List<ITimeChanging>();
+    }
+    
+    public float Time
+    {
+        get
+        {
+            return time;
+        }
+        set
+        {
+            if (value != time)
+            {
+                var delta = value - time;
+                foreach (var timeDep in TimeDependants)
+                {
+                    timeDep.AddTime(delta);
+                }
+            }
+            time = value;
+        }
+    }
+
+    public void SetTimeBruteForce(float time)
+    {
+        this.time = time;
+    }
+    
+    public IEnumerable<ITimeChanging> TimeDependants { get; set; }
+}
+
